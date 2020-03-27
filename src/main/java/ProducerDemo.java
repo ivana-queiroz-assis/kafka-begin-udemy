5dca93a794671cbce1c71e9779c0a0d230572ab3
Ivana Assis <ivanaqueirozassis@hotmail.com>
2020-03-27 07:40:56 -0300

    Inicial commit do curso de Kafka.

---
+++ src/main/java/ProducerDemo.java
@@ -0,0 +1,32 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    public static void main(String[] args) {
+
+        String bootStrapServers = "127.0.0.1:9092";
+
+        // create Produces properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+
+        //create the producer
+        KafkaProducer<String, String > producer = new KafkaProducer<String, String>(properties);
+
+        //create a producer record
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "hello world ");
+
+        //send data
+        producer.send(record);
+
+        producer.flush();
+        producer.close();
+    }
+}
